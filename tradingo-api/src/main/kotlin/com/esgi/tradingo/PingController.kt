e9ad0d0968d76f78b04f38f47d189725927727de
BAILLEUL <bailleul.sacha@gmail.com>
2021-07-05 10:33:52 +0200

    [feat]: Make prometheus grafana working

--- tradingo-api/src/main/kotlin/com/esgi/tradingo/PingController.kt
+++ tradingo-api/src/main/kotlin/com/esgi/tradingo/PingController.kt
@@ -1,10 +1,12 @@
 package com.esgi.tradingo
 
+import org.springframework.boot.CommandLineRunner
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+
 
 @RestController
 @RequestMapping("pings")
@@ -17,4 +19,23 @@ class PingController {
     fun getPing(@PathVariable message: String): ResponseEntity<PingSample>{
         return ResponseEntity.ok(PingSample(message))
     }
+
+//    @Bean
+//    fun commandLineRunner(ctx: ApplicationContext): CommandLineRunner? {
+//        return CommandLineRunner { args: Array<String?>? ->
+//            println("Let's inspect beans provided by Spring Boot:")
+//            val beanNames: Unit = ctx.getBeanDefinitionNames()
+//            Arrays.sort(beanNames)
+//            for (beanName in beanNames) {
+//                println(beanName)
+//            }
+//        }
+//    }
+//
+//    @Bean
+//    fun metricsCommonTags(): MeterRegistryCustomizer<MeterRegistry?>? {
+//        return MeterRegistryCustomizer<MeterRegistry> { registry ->
+//            registry.config().commonTags("application", "MYAPPNAME")
+//        }
+//    }
 }
--- tradingo-api/src/main/kotlin/com/esgi/tradingo/TradingoApplication.kt
+++ tradingo-api/src/main/kotlin/com/esgi/tradingo/TradingoApplication.kt
@@ -7,5 +7,5 @@ import org.springframework.boot.runApplication
 open class TradingoApplication
 
 fun main(args: Array<String>) {
-	runApplication<TradingoApplication>(*args)
+    runApplication<TradingoApplication>(*args)
 }
